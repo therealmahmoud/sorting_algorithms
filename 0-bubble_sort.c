6debaca0b3852acb83db91976837a38d2ae230db
therealmahmoud <moha91875460@gmail.com>
2024-01-20 20:13:50 +0000

    UPDATED

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,6 +18,9 @@ void bubble_sort(int *array, size_t size)
 	unsigned long int i, j;
 	int tmp;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
