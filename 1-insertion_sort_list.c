0b70df35dd8dc66615784825f9227ed6d8727d77
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-01-23 20:41:04 +0200

    “commit”

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,19 +6,19 @@
  * @n1: A pointer to the first node to swap.
  * @n2: The second node to swap.
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap(listint_t **head, listint_t **x, listint_t *y)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	(*x)->next = y->next;
+	if (y->next != NULL)
+		y->next->prev = *x;
+	y->prev = (*x)->prev;
+	y->next = *x;
+	if ((*x)->prev != NULL)
+		(*x)->prev->next = y;
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*head = y;
+	(*x)->prev = y;
+	*x = y->prev;
 }
 
 /**
@@ -30,18 +30,18 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *key, *current, *next;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	for (key = (*list)->next; key != NULL; key = next)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		next = key->next;
+		current = key->prev;
+		while (current != NULL && key->n < current->n)
 		{
-			swap_nodes(list, &insert, iter);
+			swap(list, &current, key);
 			print_list((const listint_t *)*list);
 		}
 	}
