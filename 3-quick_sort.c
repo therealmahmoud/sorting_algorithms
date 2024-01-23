27ad94c895294f0a67142525d702b9822483ba05
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-01-23 02:42:42 +0200

    “commit”

---
+++ 3-quick_sort.c
@@ -0,0 +1,82 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps the values of two integers.
+ * @x: Pointer to the first integer.
+ * @y: Pointer to the second integer.
+ */
+
+void swap(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * partition - Partitions an array for the quicksort algorithm.
+ * @array: Array to be partitioned.
+ * @low: Starting index of the array.
+ * @high: Ending index of the array.
+ * @size: Size of the array.
+ * Return: Index of the pivot element after partitioning.
+ */
+
+int partition(int *array, int low, int high, size_t size)
+{
+	int /* pivot, */ i, j;
+
+	/* pivot = high; */
+	i = low - 1;
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= array[high])
+		{
+			i++;
+			if (i < j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] > array[high])
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quicksort_recur - Recursively applies the quicksort algorithm.
+ * @array: Array to be sorted.
+ * @low: Starting index of the array.
+ * @high: Ending index of the array.
+ * @size: Size of the array.
+ */
+
+void quicksort_recur(int *array, int low, int high, size_t size)
+{
+	int pivot_idx;
+
+	if (low < high)
+	{
+		pivot_idx = partition(array, low, high, size);
+		quicksort_recur(array, low, pivot_idx - 1, size);
+		quicksort_recur(array, pivot_idx + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Initiates the quicksort algorithm on an array.
+ * @array: Array to be sorted.
+ * @size: Size of the array.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	quicksort_recur(array, 0, size - 1, size);
+}
